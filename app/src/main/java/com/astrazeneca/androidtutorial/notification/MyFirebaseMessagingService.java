8a76ccdf5dde0bd149783db683bbfdaddc23be00
Jayant Tiwari <jayant.tiwari@rinira.com>
2021-02-08 13:04:41 +0530

    Maps

--- app/src/main/java/com/astrazeneca/androidtutorial/notification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/astrazeneca/androidtutorial/notification/MyFirebaseMessagingService.java
@@ -63,6 +63,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
     }
 
+
     @Override
     public void onNewToken(String token) {
         Log.d(TAG, "Refreshed token: " + token);
